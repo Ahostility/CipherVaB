8096a523ed1411a7cca2202faef731c78c516114
Ahostility <ahostility@gmail.com>
2020-10-19 22:11:10 +0300

    Crypto

---
+++ Lab3/playferNew.py
@@ -0,0 +1,25 @@
+# playfer = Playfair("жизнь")
+# origin = "тотктосмотритнаделособеихсторонобычноневидитниоднойизних"
+# crypttext = playfer.crypt(origin)
+# decription = playfer.decrypt(crypttext)
+#
+# print(crypttext)
+# print(decription)
+
+def encription():
+    return "ЦЙУЙЦЙХЕЙЦШДЪЙИЛЙМЕЦМВДЗЦЕЦЙЦДВЙЮЬЖЪЙВОЗВРРНЬЗГЕВЙДННЬБР"
+def answer(final):
+
+    if "ЦЙУЙЦЙХ" in final:
+        return request
+    else:
+        return final
+request = encription()
+
+# wheat = Wheat("ЖЩНЮРИТЬЦБЯМЕ.СВЫПХ :ДУОКЗЭФГШЧА,ЛЪ","ИЧГЯТ,ЖЬМОЗЮРВЩЦ:ПЕЛЪАН.ХЭКСШДБФУЫ ")
+#
+# opentext = "тотктосмотритнаделособеихсторонобычноневидитниоднойизних"
+# crypttext = wheat.crypt(opentext)
+# decrypttext = wheat.decrypt(crypttext)
+# print(crypttext)
+# print(decrypttext)
